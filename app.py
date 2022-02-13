a00b33f41de9e0e0a815d437fc3c9b8997c15296
Vikren Sarkar <vikren.sarkar@gmail.com>
2022-02-13 02:03:13 -0700

    Updated graphing function

--- app.py
+++ app.py
@@ -12,14 +12,36 @@ Don\'t get cramped, let us help predict how full your flight will be. Just choos
 month of flight, and (optionally) the carrier and we will let you know how full to expect your flight to be!
 """)
 
-def plotcarriergraph(allcarrierdata):
+def plotcarriergraph(allcarrierdata, carrier_choice):
     from matplotlib.ticker import MaxNLocator
-
     xvals=[]
     yvals=[]
     carriers = allcarrierdata['Carrier'].unique()
-    for carriernum in range(len(carriers)):
-        currentcarrier = carriers[carriernum]
+    carrierloadfactors = []
+    carriernum = 0
+    for carrier in carriers:
+        datasubset = allcarrierdata[allcarrierdata['Carrier']==carrier]
+        loadfactors = datasubset['Load Factor (%)']
+        carrierloadfactors.append((carriernum, min(loadfactors)))
+        carriernum += 1
+    carrierloadfactors.sort(key=lambda x: x[1])
+    tempcarrierorder = []
+    for carriertuple in carrierloadfactors:
+        tempcarrierorder.append(carriertuple[0])
+    finalcarrierorder = []
+    if carrier_choice != 'No Selection':
+        carrierloc = np.where(carriers == carrier_choice)[0][0]
+        finalcarrierorder.append(carrierloc)
+        tempcarrierorder.remove(carrierloc)
+        finalcarrierorder.extend(tempcarrierorder)
+    else:
+        finalcarrierorder = tempcarrierorder
+    finalcarriernames = []
+    for carrier in finalcarrierorder:
+        finalcarriernames.append(carriers[carrier])
+    
+    for carriernum in range(len(finalcarrierorder)):
+        currentcarrier = finalcarriernames[carriernum]
         datasubset = allcarrierdata[allcarrierdata['Carrier']==currentcarrier]
         for carriervalue in range(datasubset.shape[0]):
             xvals.append(carriernum+0.5)
@@ -29,11 +51,10 @@ def plotcarriergraph(allcarrierdata):
     plt.ylabel("Fullness (%)", fontsize=14)
     plt.title("Fullness of all possible flights found on this route \n", fontsize=20)
     plt.xlim(0, len(carriers))
-    plt.xticks(np.arange(0.5,len(carriers)+0.5), carriers, rotation=60, fontsize=12, ha="right")
+    plt.xticks(np.arange(0.5,len(carriers)+0.5), finalcarriernames, rotation=60, fontsize=12, ha="right")
     ax = fig.gca()
     ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-    fig.savefig('CarrierFig.jpg', dpi=300, bbox_inches='tight')    
-    return 0
+    fig.savefig('CarrierFig.jpg', dpi=300, bbox_inches='tight') 
 
 Origins = pd.read_csv("Top_Airports.csv", header=None)
 origin_choice = st.sidebar.selectbox('Select your Origin Airport:', Origins, index=76)
